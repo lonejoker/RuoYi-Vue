29528a94e7c40a17015671ca538c7b5d54b74d1f
xiaobai <1582294124@qq.com>
2022-03-26 23:25:48 +0800

    测试文件上传

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
@@ -1,20 +1,20 @@
 package com.ruoyi.web.controller.system;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletResponse;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import com.ruoyi.common.config.RuoYiConfig;
+import com.ruoyi.common.utils.file.FileUploadUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.constant.UserConstants;
@@ -30,7 +30,8 @@ import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.system.service.ISysPostService;
 import com.ruoyi.system.service.ISysRoleService;
 import com.ruoyi.system.service.ISysUserService;
-
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
 /**
  * 用户信息
  * 
@@ -48,6 +49,17 @@ public class SysUserController extends BaseController
 
     @Autowired
     private ISysPostService postService;
+
+    @PostMapping("/up")
+    public String up(@RequestParam MultipartFile file) throws IOException {
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getSize());
+        System.out.println(FileUploadUtils.upload(file));
+        System.out.println(RuoYiConfig.getDownloadPath());
+        System.out.println("http://localhost:8989/common/download/resource/" + IdUtil.fastSimpleUUID() + StrUtil.DOT + FileUtil.extName(FileUploadUtils.upload(RuoYiConfig.getDownloadPath(),file)));
+        System.out.println("http://localhost:8989/common/download/resource" + FileUploadUtils.upload(file));
+        return null;
+    }
 
     /**
      * 获取用户列表
