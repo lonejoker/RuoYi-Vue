8b546caec9b4c312736b08583da7e8f2c4767bdb
xiaobai <1582294124@qq.com>
2022-03-27 19:18:19 +0800

    新增自己的模块功能

---
+++ ruoyi-ui/src/views/resources/picture/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+      Picture
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Picture',
+  components: {},
+  props: {},
+  data() {
+    return {
+        
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+      
+},
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="less" scoped>
+    
+</style>
---
+++ ruoyi-ui/src/views/resources/video/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+      video
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Video',
+  components: {},
+  props: {},
+  data() {
+    return {
+        
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+      
+},
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="less" scoped>
+    
+</style>
